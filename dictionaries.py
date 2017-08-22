d14a1acc0e2eab8d3da4dfc93e30f92d18174805
Cameron Pickle <cmpickle@gmail.com>
2017-08-22 14:57:39 -0600

    updated dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -8,10 +8,11 @@ print("print the key value of dict[\"one\"]")
 
 print(dict["one"])
 
-print("update the dictionary to binary values and print it out:")
+print("update the dictionary to binary values and add the value of four and print it out:")
 
 dict["one"] = '001'
 dict["two"] = '010'
 dict["three"] = '011'
+dict.update({"four":'100'})
 
 print(dict)
