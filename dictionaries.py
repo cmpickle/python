91b87b72e46837f19cb068b7cdbf133778765444
Cameron Pickle <cmpickle@gmail.com>
2017-08-22 14:18:16 -0600

    python dictionary examples

---
+++ dictionaries.py
@@ -0,0 +1,17 @@
+dict = { "one":1, "two":2, "three":3}
+
+print("Print out the dictionary:")
+
+print(dict)
+
+print("print the key value of dict[\"one\"]")
+
+print(dict["one"])
+
+print("update the dictionary to binary values and print it out:")
+
+dict["one"] = '001'
+dict["two"] = '010'
+dict["three"] = '011'
+
+print(dict)
