39f765b62e72ed75893257b005c6ddd2c0a1782b
Cameron Pickle <cmpickle@gmail.com>
2017-10-13 17:39:00 -0600

    added windows setup python script

---
+++ windows.py
@@ -0,0 +1,47 @@
+import pyautogui as auto
+
+print("moving chrome....")
+chrome = auto.getWindow('Google Chrome')
+if chrome != None:
+	chrome.restore()
+	chrome.move(-1920, 0)
+	chrome.maximize()
+
+print("moving Skype....")
+skype = auto.getWindow('Skype')
+if skype != None:
+	skype.restore()
+	skype.move(2873, 515)
+	skype.resize(980, 495)
+
+print("moving Slack....")
+slack = auto.getWindow('Slack')
+if slack != None:
+	slack.restore()
+	slack.move(2880, 0)
+	slack.resize(960, 520)
+
+print("moving Powershell....")
+powershell = auto.getWindow('Windows Powershell')
+if powershell != None:
+	powershell.restore()
+	powershell.move(2880, 0)
+	powershell.resize(960, 515)
+
+print("moving Visual Studio Code....")
+vsCode = auto.getWindow('Visual Studio Code')
+if vsCode != None:
+	vsCode.move(-8, -8)
+	vsCode.maximize()
+
+print("moving Visual Studio....")
+vs= auto.getWindow('Microsoft Visual Studio')
+if vs!= None:
+	vs.move(0, 0)
+	vs.maximize()
+
+print("moving Android Studio ....")
+AndroidStudio = auto.getWindow('Android Studio')
+if AndroidStudio != None:
+	AndroidStudio.move(-8, -8)
+	AndroidStudio.maximize()
